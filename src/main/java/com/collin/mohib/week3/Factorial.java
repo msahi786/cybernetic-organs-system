7d911d7bd40a3588da89d8874056990be91420a3
Mohib Sahi <mohibsahi@my.unt.edu>
2024-09-12 20:31:23 -0500

    factorial is ready

---
+++ src/main/java/com/collin/mohib/week3/Factorial.java
@@ -0,0 +1,22 @@
+package com.collin.mohib.week3;
+
+public class Factorial {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        int n = 4;
+        int fac = factorial(n);
+        System.out.println("factorial of "+n+ " is "+fac);
+    }
+    // n! = n*(n-1)*(n-2)..*2*1
+    private static int factorial(int n) {
+        System.out.print(n+" ");
+        if(n<1){
+         return 1;
+        }else {
+            return n*factorial(n - 1);
+        }
+
+    }
+
+}
